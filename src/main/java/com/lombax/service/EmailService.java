2ee9e3e229f5b5883b72b4d3438eb93d0d967d39
MartínRico <martinricomartinez@gmail.com>
2020-04-01 19:50:59 +0200

    WIP EmailService

---
+++ src/main/java/com/lombax/service/EmailService.java
@@ -0,0 +1,5 @@
+package com.lombax.service;
+
+public interface EmailService {
+    public void sendEmail(String email);
+}
---
+++ src/main/java/com/lombax/service/EmailServiceImpl.java
@@ -0,0 +1,53 @@
+package com.lombax.service;
+
+import com.lombax.data.UserModel;
+import com.lombax.exception.EntityAlreadyExistsException;
+import com.lombax.exception.EntityNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service("emailService")
+public class EmailServiceImpl implements EmailService {
+
+    private static final Logger logger = LoggerFactory.getLogger(EmailServiceImpl.class);
+
+    private final MongoTemplate mongoTemplate;
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
+    public EmailServiceImpl(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    @Async
+    @Override
+    public void sendEmail(String email) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("email").is(email));
+        UserModel user = mongoTemplate.findOne(query, UserModel.class);
+
+        if (user == null || !user.isValid()) {
+            throw new EntityNotFoundException(UserModel.class, "email", email);
+        }
+
+        logger.info("SendEmail: " + email);
+
+        // Email message
+        SimpleMailMessage passwordResetEmail = new SimpleMailMessage();
+        passwordResetEmail.setFrom("martinricomartinez@gmail.com");
+        passwordResetEmail.setTo(user.getEmail());
+        passwordResetEmail.setSubject("Password Recover Request");
+        passwordResetEmail.setText(user.getPassword());
+        mailSender.send(passwordResetEmail);
+    }
+}
