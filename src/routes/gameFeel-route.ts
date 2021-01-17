707f31d7267156252e8821395283d8d2831bba7b
mricomat <mrico@aluxion.com>
2021-01-17 14:42:09 +0100

    Fixed error

--- src/routes/gameFeel-route.ts
+++ src/routes/gameFeel-route.ts
@@ -15,48 +15,61 @@ router.post(
   "/gameFeel",
   authentication.required,
   async (req: Request, res: Response, next: NextFunction) => {
-    const gameFeel: IGameFeel = new GameFeel();
-
     const user: IUserModel = await User.findById(req.body.userId);
 
     if (!user) {
       next("Incorrect parameters");
     }
 
-    gameFeel.user = req.body.userId;
-    gameFeel.game.id = req.body.game.id;
-    gameFeel.game.imageId = req.body.game.cover.image_id;
-    gameFeel.game.releaseDate = req.body.game.first_release_date;
-    gameFeel.game.name = req.body.game.name;
-    gameFeel.gameStatus = req.body.gameStatus;
-    gameFeel.like = req.body.like;
+    const findFeel = await GameFeel.findOne({
+      user: req.body.userId,
+      "game.id": req.body.game.id,
+    });
 
-    return gameFeel
-      .save()
-      .then(() => {
-        const diary: IDiaryModel = new Diary();
-        diary.user = gameFeel.user;
-        diary.game = {
-          id: gameFeel.game.id,
-          imageId: gameFeel.game.imageId,
-        };
-        diary.gameFeel = gameFeel._id;
-        diary.type = DiaryType.GameFeel;
-        diary.action = DiaryAction.Add;
+    if (!findFeel) {
+      const gameFeel: IGameFeel = new GameFeel();
 
-        return diary
-          .save()
-          .then(async () => {
-            await user.addGameFeel(gameFeel._id);
-            await user.addDiary(diary._id);
-            return res.json({
-              gameFeel: gameFeel.toJSON(),
-              diary: diary.toJSON(),
-            });
-          })
-          .catch(next);
-      })
-      .catch(next);
+      gameFeel.user = req.body.userId;
+      gameFeel.game.id = req.body.game.id;
+      gameFeel.game.imageId = req.body.game.cover.image_id;
+      gameFeel.game.releaseDate = req.body.game.first_release_date;
+      gameFeel.game.name = req.body.game.name;
+      gameFeel.gameStatus = req.body.gameStatus;
+      gameFeel.like = req.body.like;
+
+      return gameFeel
+        .save()
+        .then(() => {
+          const diary: IDiaryModel = new Diary();
+          diary.user = gameFeel.user;
+          diary.game = {
+            id: gameFeel.game.id,
+            imageId: gameFeel.game.imageId,
+          };
+          diary.gameFeel = gameFeel._id;
+          diary.type = DiaryType.GameFeel;
+          diary.action = DiaryAction.Add;
+
+          return diary
+            .save()
+            .then(async () => {
+              await user.addGameFeel(gameFeel._id);
+              await user.addDiary(diary._id);
+              return res.json({
+                gameFeel: gameFeel.toJSON(),
+                diary: diary.toJSON(),
+              });
+            })
+            .catch(next);
+        })
+        .catch(next);
+    } else {
+      findFeel.gameStatus = req.body.gameStatus;
+      findFeel.like = req.body.like;
+      return findFeel.save().then(() => {
+        return res.json({ gameFeel: findFeel.toJSON() });
+      });
+    }
   }
 );
 
